7486017ffc3b5e29a43f0ce5e0ca8c79a1d5b1e2
Rakhal Chandra Mondal <Rakhalchandro@gmail.com>
2025-08-23 23:38:54 +0600

    Update Education.js

--- src/components/resume/Education.js
+++ src/components/resume/Education.js
@@ -17,21 +17,21 @@ const Education = () => {
         </div>
         <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
-            title="BSc in Computer Science"
-            subTitle="University of DVI (2006 - 2010)"
-            result="3.90/4"
+            title="Bachelor of Social Science"
+            subTitle="Jhaudanga College, Satkhira - (2010 - 2014)"
+            result="2.64/4"
             des="The training provided by universities in order to prepare people to work in various sectors of the economy or areas of culture."
           />
           <ResumeCard
-            title="AS - Science & Information"
-            subTitle="SuperKing College (2001 - 2005)"
-            result="4.75/5"
+            title="Higher Secondary School"
+            subTitle="Kaloroya Gov't College, Satkhira - (2010 - 2012)"
+            result="3.80/5"
             des="Higher education is tertiary education leading to award of an academic degree. Higher education, also called post-secondary education."
           />
           <ResumeCard
             title="Secondary School Education"
-            subTitle="Kingstar Secondary School (1998 - 2000)"
-            result="5.00/5"
+            subTitle="Tengrakhali High School, Satkhira - (2008 - 2010)"
+            result="3.50/5"
             des="Secondary education or post-primary education covers two phases on the International Standard Classification of Education scale."
           />
         </div>
@@ -45,22 +45,22 @@ const Education = () => {
         </div>
         <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
-            title="Sr. Software Engineer"
-            subTitle="Google Out Tech - (2017 - Present)"
-            result="USA"
-            des="Google's hiring process is an important part of our culture. Googlers care deeply about their teams and the people who make them up."
+            title="Joykoly Publications Ltd."
+            subTitle="Office executive - (2025 - Present)"
+            result="Dhaka, Bangladesh "
+            des="I was working as a office executive in Joykoly Publications Ltd. I was responsible for the office work and other duties."
           />
           <ResumeCard
             title="Web Developer & Trainer"
-            subTitle="Apple Developer Team - (2012 - 2016)"
-            result="MALAYSIA"
-            des="A popular destination with a growing number of highly qualified homegrown graduates, it's true that securing a role in Malaysia isn't easy."
+            subTitle="RC Training center - (2015 - 2024)"
+            result="Assasuni, Satkhira, Bangladesh"
+            des="I was working as a trainer in RC Training center. I was responsible for the training of the students."
           />
           <ResumeCard
-            title="Front-end Developer"
-            subTitle="Nike - (2020 - 2011)"
-            result="Oman"
-            des="The Oman economy has grown strongly over recent years, having transformed itself from a producer and innovation-based economy."
+            title="Robi axiata limited"
+            subTitle="Office executive - (2011 - 2014)"
+            result="Satkhira, Bangladesh "
+            des="I was working as a office executive in robi axiata limited. I was responsible for the office work and other duties."
           />
         </div>
       </div>
